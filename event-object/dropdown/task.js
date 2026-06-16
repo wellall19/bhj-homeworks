cf06d932179234f7e88e4e047cdf627953c56456
Maria Tomilova <m.tomilova2802@gmail.com>
2026-06-16 20:41:54 +0300

    Second commit event-object

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,19 +1,19 @@
-const dropdownValue = document.querySelector('.dropdown__value');
-const list = document.querySelector('ul.dropdown__list');
+const dropdownValues = Array.from(document.querySelectorAll('.dropdown__value'));
+const lists = Array.from(document.querySelectorAll('ul.dropdown__list'));
 const dropdownItems = Array.from(document.querySelectorAll('li.dropdown__item'));
 
 function dropdownList() { 
-    list.classList.toggle('dropdown__list_active');
+    lists.forEach((el) => el.classList.toggle('dropdown__list_active'));
 }
 
 function choiceItem(event) {
     event.preventDefault();
     const clickedItem = event.target.closest('.dropdown__item');
-    dropdownValue.textContent = clickedItem.textContent;
-    list.classList.remove('dropdown__list_active');
+    dropdownValues.forEach((el) => el.textContent = clickedItem.textContent);
+    lists.forEach((el) => el.classList.remove('dropdown__list_active'));
 }
 
-dropdownValue.addEventListener('click', dropdownList);
+dropdownValues.forEach((el) => el.addEventListener('click', dropdownList));
 dropdownItems.forEach((item) => { 
     item.addEventListener('click', choiceItem) 
 });
--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,21 +1,23 @@
-const listOfTabs = Array.from(document.querySelectorAll('.tabs .tab__navigation .tab'));
-const listOfTabsContents = Array.from(document.querySelectorAll('.tabs .tab__content'));
+const listOfTabs = Array.from(document.querySelectorAll('.tab'));
 
 function clickOnTab(event){
-    const activeTab = document.querySelector('.tabs .tab_active');
+    const parentOfActiveTab = event.target.closest('.tabs');
+    const activeTab = parentOfActiveTab.querySelector('.tab_active');
     if (activeTab === event.target) return;
     
     activeTab.classList.remove('tab_active');
     event.target.classList.add('tab_active');
 
-    const n = listOfTabs.findIndex(tab => tab.textContent === event.target.textContent);
-    const activeTabContent = document.querySelector('.tabs .tab__content_active');
+    const currentListOfTabs = Array.from(parentOfActiveTab.querySelectorAll('.tab'));
+    const currentListOfTabsContents = Array.from(parentOfActiveTab.querySelectorAll('.tab__content'))
+
+    const n = currentListOfTabs.indexOf(event.target);
+    const activeTabContent = parentOfActiveTab.querySelector('.tab__content_active');
     
     activeTabContent.classList.remove('tab__content_active');
-    listOfTabsContents[n].classList.add('tab__content_active');
+    currentListOfTabsContents[n].classList.add('tab__content_active');
 }
 
 listOfTabs.forEach((tab) => {
     tab.addEventListener('click', clickOnTab);
-    
 })
